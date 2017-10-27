c7333ac2c0d3e87f4eda2c32279597fafa1b3e2e
mohamed-bahaj <Mohamed.BAHAJ@student.umons.ac.be>
2017-10-27 14:39:45 +0200

    exo3

---
+++ src/main/java/exercise3/ConvexPolygon.java
@@ -0,0 +1,14 @@
+package exercise3;
+
+public class ConvexPolygon implements Drawable {
+	private final Point[] points;
+
+	public ConvexPolygon(Point[] points){
+		this.points = points;
+	}
+
+	@Override
+	public Point[] getVertices(){
+		return points;
+	}
+}
--- src/main/java/exercise3/Exercise3.java
+++ src/main/java/exercise3/Exercise3.java
@@ -2,6 +2,12 @@ package exercise3;
 
 public class Exercise3 {
 	public static void main(String[] args) {
-		new Panel(new Drawable[0]);
+		new Panel(new Drawable[] {
+			new ConvexPolygon(new Point[] {
+				new Point(10, 10),
+					new Point(200, 200),
+					new Point(300, 10)
+			})
+		});
 	}
 }
